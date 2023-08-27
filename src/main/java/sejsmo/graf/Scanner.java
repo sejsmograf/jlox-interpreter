23b7f16d473dbdc3f81d9536a8e3d3dbc0d21a69
=Tomasz Mroczko <=tomekmr2001@gmail.com>
2023-08-27 16:56:09 +0200

    added block comments

--- src/main/java/sejsmo/graf/Scanner.java
+++ src/main/java/sejsmo/graf/Scanner.java
@@ -1,9 +1,7 @@
 package sejsmo.graf;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static sejsmo.graf.TokenType.*;
 
@@ -13,33 +11,10 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
-  private static final Map<String, TokenType> keywords;
-
-  static {
-    keywords = new HashMap<>();
-    keywords.put("and",    AND);
-    keywords.put("class",  CLASS);
-    keywords.put("else",   ELSE);
-    keywords.put("false",  FALSE);
-    keywords.put("for",    FOR);
-    keywords.put("fun",    FUN);
-    keywords.put("if",     IF);
-    keywords.put("nil",    NIL);
-    keywords.put("or",     OR);
-    keywords.put("print",  PRINT);
-    keywords.put("return", RETURN);
-    keywords.put("super",  SUPER);
-    keywords.put("this",   THIS);
-    keywords.put("true",   TRUE);
-    keywords.put("var",    VAR);
-    keywords.put("while",  WHILE);
-  }
-
 
   Scanner(String source) {
     this.source = source;
   }
-
 
   List<Token> scanTokens() {
     while (!isAtEnd()) {
@@ -101,10 +76,13 @@ class Scanner {
 
       case '/':
         if (match('/')) {
-          // A comment goes until the end of the line.
-          while (peek() != '\n' && !isAtEnd())
-            advance();
-        } else {
+          lineComment();
+        } else if (match('*')) {
+          blockComment();
+        }
+
+        else {
+
           addToken(SLASH);
         }
         break;
@@ -135,16 +113,25 @@ class Scanner {
     }
   }
 
-  private void identifier() {
-    while (isAlphaNumeric(peek()))
+  private void blockComment(){
+    while (peek() != '*' && peekNext() != '/'){
+      if(peek() == '\n'){
+        line++;
+      }
+      if (isAtEnd()){
+        Lox.error(line, "Unterminated comment");
+        return;
+      }
       advance();
+    }
+    advance();
+    advance();
+  }
 
-    String text = source.substring(start, current);
-    TokenType type = keywords.get(text);
-    if(type == null) type = IDENTIFIER;
-
-    addToken(type);
-
+  private void lineComment() {
+    // A comment goes until the end of the line.
+    while (peek() != '\n' && !isAtEnd())
+      advance();
   }
 
   private void number() {
@@ -158,6 +145,13 @@ class Scanner {
         advance();
     }
     addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) {
+      return '\0';
+    }
+    return source.charAt(current + 1);
   }
 
   private void string() {
@@ -176,7 +170,7 @@ class Scanner {
     String value = source.substring(start + 1, current - 1);
     addToken(STRING, value);
   }
-  
+
   private boolean match(char expected) {
     if (isAtEnd())
       return false;
@@ -186,18 +180,11 @@ class Scanner {
     current++;
     return true;
   }
-  
+
   private char peek() {
     if (isAtEnd())
       return '\0';
     return source.charAt(current);
-  }
-  
-  private char peekNext() {
-    if (current + 1 >= source.length()) {
-      return '\0';
-    }
-    return source.charAt(current + 1);
   }
 
   private boolean isAtEnd() {
@@ -217,6 +204,13 @@ class Scanner {
     tokens.add(new Token(type, text, literal, line));
   }
 
+  private void identifier() {
+    while (isAlphaNumeric(peek()))
+      advance();
+
+    addToken(IDENTIFIER);
+
+  }
 
   private boolean isAlpha(char c) {
     return (c >= 'a' && c <= 'z') ||
@@ -227,10 +221,5 @@ class Scanner {
   private boolean isAlphaNumeric(char c) {
     return isAlpha(c) || Character.isDigit(c);
   }
-
-
-
-
-
 
 }
